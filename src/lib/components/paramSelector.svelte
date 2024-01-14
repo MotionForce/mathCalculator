3073355697b3b5258034cac98129f14e499359e1
MotionForce <74267742+MotionForce@users.noreply.github.com>
2024-01-14 16:17:46 -0500

    final

--- src/lib/components/paramSelector.svelte
+++ src/lib/components/paramSelector.svelte
@@ -1,24 +1,100 @@
 <script lang="ts">
   import Katex from "svelte-katex";
+  import { RangeSlider } from "@skeletonlabs/skeleton";
+
+  function updateH(b: number) {
+    h = -b / (2 * a);
+  }
+
+  function updateK(c: number) {
+    console.log(c);
+    k = (4 * a * c - b ** 2) / (4 * a);
+  }
+
+  export let precision: number = 5;
 
   export let a: number = 0;
-  export let h = 0;
-  export let k = 0;
+  export let h: number = 0;
+  export let k: number = 0;
+
+  export let forme: "canonique" | "generale" = "canonique";
+
+  let b: number = -2 * a * h;
+  let c: number = a * h ** 2 + k;
 </script>
 
-<h2 class="h2">Entrer les variables de la forme canonique (a, h et k).</h2>
-<div class="flex flex-col lg:flex-row lg:space-x-5">
-  <label class="label">
-    <span>a</span>
-    <input type="number" title="Entrer a" class="input" bind:value={a} />
-  </label>
-  <label class="label">
-    <span>h</span>
-    <input type="number" title="Entrer h" class="input" bind:value={h} />
-  </label>
-  <label class="label">
-    <span>k</span>
-    <input type="number" title="Entrer k" class="input" bind:value={k} />
-  </label>
-</div>
-<Katex>f(x)={a}(x{h > 0 ? "-" : "+"}{h})^2{k > 0 ? "+" : "-"}{k}</Katex>
+<select class="select" bind:value={forme}>
+  <option value="canonique">Forme canonique</option>
+  <option value="generale">Forme générale</option>
+</select>
+<RangeSlider
+  name="range-slider"
+  bind:value={precision}
+  min={1}
+  max={10}
+  step={1}
+  ticked
+>
+  <div class="flex justify-between items-center">
+    <div class="font-bold">Nombre de decimales</div>
+    <div class="text-xs">{precision - 1} / 9</div>
+  </div>
+</RangeSlider>
+<hr />
+{#if forme === "canonique"}
+  <h2 class="h2">Entrer les variables de la forme canonique (a, h et k).</h2>
+  <div class="flex flex-col lg:flex-row lg:space-x-5">
+    <label class="label">
+      <span>a</span>
+      <input type="number" title="Entrer a" class="input" bind:value={a} />
+    </label>
+    <label class="label">
+      <span>h</span>
+      <input type="number" title="Entrer h" class="input" bind:value={h} />
+    </label>
+    <label class="label">
+      <span>k</span>
+      <input type="number" title="Entrer k" class="input" bind:value={k} />
+    </label>
+  </div>
+  <Katex
+    >f(x)={a.toPrecision(precision)}(x{h > 0 ? "-" : "+"}{h.toPrecision(
+      5,
+    )})^2{k > 0 ? "+" : ""}{k.toPrecision(5)}</Katex
+  >
+{:else}
+  <h2 class="h2">Entrer les variables de la forme générale (a, b et c).</h2>
+  <div class="flex flex-col lg:flex-row lg:space-x-5">
+    <label class="label">
+      <span>a</span>
+      <input type="number" title="Entrer a" class="input" bind:value={a} />
+    </label>
+    <label class="label">
+      <span>b</span>
+      <input
+        type="number"
+        title="Entrer b"
+        class="input"
+        bind:value={b}
+        on:change={() => updateH(b)}
+        on:input={() => updateH(b)}
+      />
+    </label>
+    <label class="label">
+      <span>c</span>
+      <input
+        type="number"
+        title="Entrer c"
+        class="input"
+        bind:value={c}
+        on:change={() => updateK(c)}
+        on:input={() => updateK(c)}
+      />
+    </label>
+  </div>
+  <Katex
+    >f(x)={a.toPrecision(precision)}x^2{b > 0 ? "+" : "-"}{b.toPrecision(
+      precision,
+    )}x{c > 0 ? "+" : ""}{c.toPrecision(precision)}</Katex
+  >
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,40 +4,204 @@
   import { storage } from "svelte-legos";
   import { writable } from "svelte/store";
 
+  let precision = storage(writable(5), "precision");
+
   let a = storage(writable(1), "a");
   let h = storage(writable(0), "h");
   let k = storage(writable(0), "k");
 
-  $: min = a > 0 ? k : null;
-  $: max = a < 0 ? k : null;
+  let forme = storage(writable<"canonique" | "generale">("canonique"), "forme");
+
+  $: precision_ = $precision < 1 || $precision > 10 ? 5 : $precision;
+  $: r_a = parseFloat($a.toPrecision(precision_));
+  $: r_h = parseFloat($h.toPrecision(precision_));
+  $: r_k = parseFloat($k.toPrecision(precision_));
+  $: sol_1 =
+    $a !== 0
+      ? parseFloat(($h - Math.sqrt(-$k / $a)).toPrecision(precision_))
+      : null;
+  $: sol_2 =
+    $a !== 0
+      ? parseFloat(($h + Math.sqrt(-$k / $a)).toPrecision(precision_))
+      : null;
 </script>
 
 <div
-  class="m-10 p-10 flex flex-col space-y-10 bg-surface-300-600-token rounded-3xl"
+  class="lg:m-10 p-4 md:p-10 flex flex-col space-y-10 bg-surface-300-600-token lg:rounded-3xl"
 >
   <h1 class="h1 font-bold">
     Calculatrice de propriété de fonction quadratique
   </h1>
   <div class="p-7 flex flex-col space-y-5 bg-surface-500-400-token rounded-2xl">
-    <ParamSelector bind:a={$a} bind:h={$h} bind:k={$k} />
-    {#if a !== 0}
-      <hr />
-      <h2 class="h2">Les propriétés</h2>
-      <Katex>dom(f)=\mathbb{"{R}"}</Katex>
-      <Katex
-        >ima(f)={min === null ? `]-\\infty,${k}]` : `[${min},+\\infty[`}</Katex
-      >
-      <Katex>f(0)={(-h) ** 2 * a + k}</Katex>
-      <Katex>
-        {#if k === 0}
-          f(x)=0\space\forall\space x={h}
-        {:else if a * k > 0}
-          f(x)=0\space\forall\space x\in{"\\{\\}"}
-        {:else}
-          f(x)=0\space\forall\space x\in{"\\{"}{h - Math.sqrt(-k / a)},{h +
-            Math.sqrt(-k / a)}{"\\}"}
-        {/if}
-      </Katex>
+    <ParamSelector
+      bind:precision={precision_}
+      bind:a={$a}
+      bind:h={$h}
+      bind:k={$k}
+      bind:forme={$forme}
+    />
+    {#if typeof $a === "number" && typeof $h === "number" && typeof $k === "number"}
+      {#if $a !== 0}
+        <hr />
+        <h2 class="h2">Les propriétés</h2>
+        <div class="table-container">
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th class="w-fit min-w-56">Propriété</th>
+                <th class="w-full">Valeur</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>Domaine</th>
+                <td>
+                  <Katex>dom(f)=\mathbb{"{R}"}</Katex>
+                </td>
+              </tr>
+              <tr>
+                <th>Image</th>
+                <td>
+                  <Katex
+                    >ima(f)={r_a > 0 === true
+                      ? `]-\\infty,${r_k}]`
+                      : `[${r_k},+\\infty[`}</Katex
+                  >
+                </td>
+              </tr>
+              <tr>
+                <th>Valeur initiale</th>
+                <td>
+                  <Katex
+                    >f(0)={parseFloat(
+                      ((-$h) ** 2 * $a + $k).toPrecision(precision_),
+                    )}</Katex
+                  >
+                </td>
+              </tr>
+              <tr>
+                <th>Zéros</th>
+                <td>
+                  <Katex>
+                    {#if $k === 0}
+                      f(x)=0\space\forall\space x={r_h}
+                    {:else if $a * $k > 0}
+                      f(x)=0\space\forall\space x\in{"\\{\\}"}
+                    {:else}
+                      f(x)=0\space\forall\space x\in{"\\{"}{sol_1},{sol_2}{"\\}"}
+                    {/if}
+                  </Katex>
+                </td>
+              </tr>
+              <tr>
+                <th>Croissance</th>
+                <td>
+                  {#if $a > 0}
+                    <Katex>
+                      f\nearrow\space\forall\space x\in{`[${r_h},+\\infty[`}
+                    </Katex>
+                  {:else}
+                    <Katex>
+                      f\nearrow\space\forall\space x\in{`]-\\infty,${r_h}]`}
+                    </Katex>
+                  {/if}
+                </td>
+              </tr>
+              <tr>
+                <th>Décroissance</th>
+                <td>
+                  {#if $a > 0}
+                    <Katex>
+                      f\searrow\space\forall\space x\in{`]-\\infty,${r_h}]`}
+                    </Katex>
+                  {:else}
+                    <Katex>
+                      f\searrow\space\forall\space x\in{`[${r_h},+\\infty[`}
+                    </Katex>
+                  {/if}
+                </td>
+              </tr>
+              <tr>
+                <th>Maximum</th>
+                <td>
+                  {#if $a > 0}
+                    <p class="text-lg">aucun</p>
+                  {:else}
+                    <Katex>{r_k}</Katex>
+                  {/if}
+                </td>
+              </tr>
+              <tr>
+                <th>Minimum</th>
+                <td>
+                  {#if $a > 0}
+                    <Katex>{r_k}</Katex>
+                  {:else}
+                    <p class="text-lg">aucun</p>
+                  {/if}
+                </td>
+              </tr>
+              <tr>
+                <th>Signe positif</th>
+                <td>
+                  {#if $a >= 0 && $k >= 0}
+                    <Katex>
+                      f(x)\geq0\space\forall\space x\in\mathbb{"{R}"}
+                    </Katex>
+                  {:else if $a < 0 && $k < 0}
+                    <Katex>
+                      f(x)\geq0\space\forall\space x\in\mathbb{"\\{\\}"}
+                    </Katex>
+                  {:else if $a < 0 && $k === 0}
+                    <Katex>
+                      f(x)\geq0\space\forall\space x={r_h}
+                    </Katex>
+                  {:else if $a < 0 && $k > 0}
+                    <Katex>
+                      f(x)\geq0\space\forall\space x\in[{sol_1},{sol_2}]
+                    </Katex>
+                  {:else if $a > 0 && $k < 0}
+                    <Katex>
+                      f(x)\geq0\space\forall\space x\in]-\infty,{sol_1}]\cup[{sol_2},+\infty[
+                    </Katex>
+                  {/if}
+                </td>
+              </tr>
+              <tr>
+                <th>Signe négatif</th>
+                <td>
+                  {#if $a > 0 && $k > 0}
+                    <Katex>
+                      f(x)\leq0\space\forall\space x\in\mathbb{"\\{\\}"}
+                    </Katex>
+                  {:else if $a <= 0 && $k <= 0}
+                    <Katex>
+                      f(x)\leq0\space\forall\space x\in\mathbb{"{R}"}
+                    </Katex>
+                  {:else if $a > 0 && $k === 0}
+                    <Katex>
+                      f(x)\leq0\space\forall\space x={r_h}
+                    </Katex>
+                  {:else if $a > 0 && $k < 0}
+                    <Katex>
+                      f(x)\leq0\space\forall\space x\in[{sol_1},{sol_2}]
+                    </Katex>
+                  {:else if $a < 0 && $k > 0}
+                    <Katex>
+                      f(x)\leq0\space\forall\space x\in]-\infty,{sol_1}]\cup[{sol_2},+\infty[
+                    </Katex>
+                  {/if}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      {/if}
+    {:else}
+      <h4 class="h4">
+        S'il vous plaît inscrivez les valeurs des paramètres a, h et k au
+        dessus.
+      </h4>
     {/if}
   </div>
 </div>
