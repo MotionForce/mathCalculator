89e8dfb6296a5c4917b7032c97a7ea05c6ed9fd0
MotionForce <74267742+MotionForce@users.noreply.github.com>
2026-03-20 16:17:46 -0400

    non math deps

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,13 @@
 <script lang="ts">
     import "../app.css";
-    import type { ModalSettings } from "@skeletonlabs/skeleton-svelte";
-    import Credits from "$lib/components/credits.svelte";
+    import { Dialog, Portal } from "@skeletonlabs/skeleton-svelte";
     interface Props {
         children?: import("svelte").Snippet;
     }
 
     let { children }: Props = $props();
-
-    const creditsModal: ModalSettings = {
-        type: "component",
-        component: { ref: Credits },
-        title: "Bibliothèques et librairies utilisées",
-    };
-
-    initializeStores();
-
-    const modalStore = getModalStore();
-
-    function openCreditsModal() {
-        modalStore.trigger(creditsModal);
-    }
 </script>
 
-<Modal />
 {@render children?.()}
 
 <footer>
@@ -31,9 +15,92 @@
         Site-web créé par Andrey Kovalskiy. Pour tout problème contactez moi à
         l'adresse
         <i>andrey.kovalskiy@outlook.com</i>.
-        <button onclick={openCreditsModal}>
-            <u>Crédits</u>
-        </button>.
+        <Dialog role="alertdialog">
+            <Dialog.Trigger class="btn preset-filled">Crédits</Dialog.Trigger>
+            <Portal>
+                <Dialog.Backdrop
+                    class="fixed inset-0 z-50 bg-error-50-950/50"
+                />
+                <Dialog.Positioner
+                    class="fixed inset-0 z-50 flex justify-center items-center"
+                >
+                    <Dialog.Content
+                        class="card preset-filled-error-500 w-md p-4 space-y-2 shadow-xl"
+                    >
+                        <Dialog.Title class="text-2xl font-bold"
+                            >Bibliothèques et librairies utilisées</Dialog.Title
+                        >
+                        <Dialog.Description>
+                            <p>Pour la structure du site-web :</p>
+                            <ul class="list">
+                                <li>
+                                    <a href="https://svelte.dev/"
+                                        ><u>Svelte et SvelteKit</u></a
+                                    >
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://github.com/ankurrsinghal/svelte-legos"
+                                        ><u>Svelte Legos</u></a
+                                    > (seulement pour la version précédente, pas
+                                    la version actuelle
+                                </li>
+                            </ul>
+                            <div
+                                class="w-full border-t-2 border-gray-600"
+                            ></div>
+                            <p>Pour l'interface utilisateur :</p>
+                            <ul class="list">
+                                <li>
+                                    <a href="https://www.skeleton.dev/"
+                                        ><u>Skeleton</u></a
+                                    >
+                                </li>
+                                <li>
+                                    <a href="https://tailwindcss.com/"
+                                        ><u>TailwindCSS</u></a
+                                    >
+                                </li>
+                            </ul>
+                            <div
+                                class="w-full border-t-2 border-gray-600"
+                            ></div>
+                            <p>
+                                Pour toutes les fonctionalités mathématiques :
+                            </p>
+                            <ul class="list">
+                                <li>
+                                    <a href="https://plotly.com/javascript/"
+                                        ><u>Plotly</u></a
+                                    >
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://github.com/aknakos/sveltekit-plotly"
+                                        ><u>Svelte Plotly wrapper</u></a
+                                    >
+                                </li>
+                                <li>
+                                    <a href="https://mathjs.org/"
+                                        ><u>MathJS</u></a
+                                    >
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://github.com/kelvinsjk/mathlified/tree/main/packages/svelte-katex"
+                                        ><u>Svelte-Katex</u></a
+                                    >
+                                </li>
+                            </ul>
+                        </Dialog.Description>
+                        <Dialog.CloseTrigger class="btn preset-tonal-error"
+                            >Fermer</Dialog.CloseTrigger
+                        >
+                    </Dialog.Content>
+                </Dialog.Positioner>
+            </Portal>
+        </Dialog>
+
         <a href="https://github.com/MotionForce/mathCalculator">
             Code source sur <u>Github</u>.
         </a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    import { run } from 'svelte/legacy';
+    import { run } from "svelte/legacy";
 
     // @ts-expect-error
     import Katex from "svelte-katex";
     import ParamSelector from "$lib/components/paramSelector.svelte";
-    import {writable} from "svelte/store";
+    import { writable } from "svelte/store";
     import Plot from "$lib/components/plot.svelte";
     import {
         unequal,
@@ -24,16 +24,21 @@
         signe_positif,
         signe_negatif,
     } from "$lib/properties";
-    import {calc_sols, format_decimal} from "$lib/utilities";
-    import {dev} from "$app/environment";
+    import { calc_sols, format_decimal } from "$lib/utilities";
+    import { dev } from "$app/environment";
 
     const string_a = writable<string>("1");
     const string_h = writable<string>("0");
     const string_k = writable<string>("0");
-    const forme = writable<"canonique" | "generale" | "factorisee">("canonique");
+    const forme = writable<"canonique" | "generale" | "factorisee">(
+        "canonique",
+    );
 
+    // @ts-expect-error
     let a: Fraction | null = $state();
+    // @ts-expect-error
     let h: Fraction | null = $state();
+    // @ts-expect-error
     let k: Fraction | null = $state();
 
     try {
@@ -80,39 +85,42 @@
             k = null;
         }
     });
-    let num_type =
-        $derived(typeOf(a) === "Fraction" &&
-        typeOf(h) === "Fraction" &&
-        typeOf(k) === "Fraction" &&
-        a !== null &&
-        h !== null &&
-        k !== null);
+    let num_type = $derived(
+        typeOf(a) === "Fraction" &&
+            typeOf(h) === "Fraction" &&
+            typeOf(k) === "Fraction" &&
+            a !== null &&
+            h !== null &&
+            k !== null,
+    );
 </script>
 
 {#if dev}
     {`$a: ${string_a} ${typeof string_a}, $h: ${string_h} ${typeof string_h}, $k: ${string_k} ${typeof string_k}`}
-    <br/>
+    <br />
     {`$a: ${a} ${typeof a}, $h: ${h} ${typeof h}, $k: ${k} ${typeof k}`}
-    <br/>
+    <br />
     {`sol_1: ${sols[0]} ${typeof sols[0]}, sol_2: ${sols[1]} ${typeof sols[1]}`}
-    <br/>
+    <br />
     {`forme: ${$forme} ${typeof $forme}`}
 {/if}
 <div
-        class="lg:m-10 p-4 md:p-10 flex flex-col space-y-10 bg-surface-300-700 lg:rounded-3xl"
+    class="lg:m-10 p-4 md:p-10 flex flex-col space-y-10 bg-surface-300-700 lg:rounded-3xl"
 >
     <h1 class="h1 font-bold">
         Calculatrice de propriétés de fonction quadratique
     </h1>
     <div class="p-7 flex flex-col space-y-5 bg-surface-600-400 rounded-2xl">
-        <h4 class="h4">S'il-vous-plait utiliser des points à la place des virgules.</h4>
+        <h4 class="h4">
+            S'il-vous-plait utiliser des points à la place des virgules.
+        </h4>
         <ParamSelector
-                bind:a={$string_a}
-                bind:h={$string_h}
-                bind:k={$string_k}
+            bind:a={$string_a}
+            bind:h={$string_h}
+            bind:k={$string_k}
         />
         {#if num_type && a !== null && h !== null && k !== null && unequal(a, 0)}
-            <hr/>
+            <hr />
             <h2 class="h2">Les propriétés</h2>
             <div class="flex flex-col xl:flex-row space-x-3">
                 <!-- table -->
@@ -120,82 +128,122 @@
                     {#key a}
                         {#key h}
                             {#key k}
-                                <table class="table ">
+                                <table class="table">
                                     <thead>
-                                    <tr>
-                                        <th class="w-fit min-w-56">Propriété</th>
-                                        <th class="w-full">Valeur</th>
-                                    </tr>
+                                        <tr>
+                                            <th class="w-fit min-w-56"
+                                                >Propriété</th
+                                            >
+                                            <th class="w-full">Valeur</th>
+                                        </tr>
                                     </thead>
                                     <tbody>
-                                    <tr>
-                                        <th>Domaine</th>
-                                        <td>
-                                            <Katex>{domain()}</Katex>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th>Image</th>
-                                        <td>
-                                            <Katex>{image(a, k)}</Katex>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th>Valeur initiale</th>
-                                        <td>
-                                            <Katex>{valeur_initiale(a, h, k)}</Katex>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th>Zéros</th>
-                                        <td>
-                                            <Katex>{zeros(a, h, k, sols)}</Katex>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th>Croissance</th>
-                                        <td>
-                                            <Katex>{croissance(a, h)}</Katex>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th>Décroissance</th>
-                                        <td>
-                                            <Katex>{decroissance(a, h)}</Katex>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th>Maximum</th>
-                                        <td>
-                                            {#if larger(a, 0)}
-                                                <p class="text-lg">aucun</p>
-                                            {:else}
-                                                <Katex>{format_decimal(k)}</Katex>
-                                            {/if}
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th>Minimum</th>
-                                        <td>
-                                            {#if larger(a, 0)}
-                                                <Katex>{format_decimal(k)}</Katex>
-                                            {:else}
-                                                <p class="text-lg">aucun</p>
-                                            {/if}
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th>Signe positif</th>
-                                        <td>
-                                            <Katex>{signe_positif(a, h, k, sols)}</Katex>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th>Signe négatif</th>
-                                        <td>
-                                            <Katex>{signe_negatif(a, h, k, sols)}</Katex>
-                                        </td>
-                                    </tr>
+                                        <tr>
+                                            <th>Domaine</th>
+                                            <td>
+                                                <Katex>{domain()}</Katex>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Image</th>
+                                            <td>
+                                                <Katex>{image(a, k)}</Katex>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Valeur initiale</th>
+                                            <td>
+                                                <Katex
+                                                    >{valeur_initiale(
+                                                        a,
+                                                        h,
+                                                        k,
+                                                    )}</Katex
+                                                >
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Zéros</th>
+                                            <td>
+                                                <Katex
+                                                    >{zeros(
+                                                        a,
+                                                        h,
+                                                        k,
+                                                        sols,
+                                                    )}</Katex
+                                                >
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Croissance</th>
+                                            <td>
+                                                <Katex>{croissance(a, h)}</Katex
+                                                >
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Décroissance</th>
+                                            <td>
+                                                <Katex
+                                                    >{decroissance(a, h)}</Katex
+                                                >
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Maximum</th>
+                                            <td>
+                                                {#if larger(a, 0)}
+                                                    <p class="text-lg">aucun</p>
+                                                {:else}
+                                                    <Katex
+                                                        >{format_decimal(
+                                                            k,
+                                                        )}</Katex
+                                                    >
+                                                {/if}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Minimum</th>
+                                            <td>
+                                                {#if larger(a, 0)}
+                                                    <Katex
+                                                        >{format_decimal(
+                                                            k,
+                                                        )}</Katex
+                                                    >
+                                                {:else}
+                                                    <p class="text-lg">aucun</p>
+                                                {/if}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Signe positif</th>
+                                            <td>
+                                                <Katex
+                                                    >{signe_positif(
+                                                        a,
+                                                        h,
+                                                        k,
+                                                        sols,
+                                                    )}</Katex
+                                                >
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Signe négatif</th>
+                                            <td>
+                                                <Katex
+                                                    >{signe_negatif(
+                                                        a,
+                                                        h,
+                                                        k,
+                                                        sols,
+                                                    )}</Katex
+                                                >
+                                            </td>
+                                        </tr>
                                     </tbody>
                                 </table>
                             {/key}
@@ -208,7 +256,7 @@
                         {#key h}
                             {#key k}
                                 {#if num_type && unequal(a, 0)}
-                                    <Plot {a} {h} {k}/>
+                                    <Plot {a} {h} {k} />
                                 {/if}
                             {/key}
                         {/key}
@@ -217,8 +265,8 @@
             </div>
         {:else}
             <h4 class="h4">
-                S'il vous plaît inscrivez les valeurs numériques pour les paramètres
-                ci-dessus.
+                S'il vous plaît inscrivez les valeurs numériques pour les
+                paramètres ci-dessus.
             </h4>
         {/if}
     </div>
